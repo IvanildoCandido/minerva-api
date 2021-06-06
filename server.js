aa5b0590956da81ea690f15ca15731dd2bfcc876
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-06-06 14:50:12 -0300

    Criaçao das rotas

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const fileupload = require('express-fileupload');
+const apiRoutes = require('./src/routes');
 
 const OPTIONS = {
   useNewUrlParser: true,
@@ -24,9 +25,7 @@ server.use(fileupload());
 
 server.use(express.static(`${__dirname}/public`));
 
-server.get('/ping', (req, res) => {
-  res.json({ pong: true });
-});
+server.use('/', apiRoutes);
 
 server.listen(process.env.PORT, () => {
   console.log(`Servidor rodando no endereço: ${process.env.BASE}`);
